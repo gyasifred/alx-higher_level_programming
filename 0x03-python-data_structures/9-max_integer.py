e674c52fcfaf52b3b9c33676902fb8c02d36615c
gyasifred <gyasifred@gmail.com>
2023-06-13 13:48:54 +0000

    maximun number of a list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if not my_list:
-        return None
-    max_num = 0
+        return
+    max_num = my_list[0]
     for num in my_list:
         if num > my_list:
             max_num = num
+        else:
+            continue
     return max_num
