962553de95339e7c0477bdf638b6e94bfae95365
gyasifred <gyasifred@gmail.com>
2023-06-18 07:10:41 +0000

    function that returns a key with the biggest integer value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,5 +2,4 @@
 def best_score(a_dictionary):
     if len(a_dictionary) == 0:
         return None
-    max_key = max(a_dictionary, key=lambda k: a_dictionary[k])
-    return max_key
+    return (max(a_dictionary, key=lambda k: a_dictionary[k]))
