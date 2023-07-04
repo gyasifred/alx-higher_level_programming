8405c92740d68e22a0b00785c2f1d5f09ff38ddc
gyasifred <gyasifred@gmail.com>
2023-07-04 10:39:03 +0000

    getter and setter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a Square"""
+
+    def __init__(self, size=0):
+        ''' Initialise a new square class
+        Args:
+            size (int): The size of square
+        '''
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        '''Calculate Area of Square'''
+        return self.__size ** 2
