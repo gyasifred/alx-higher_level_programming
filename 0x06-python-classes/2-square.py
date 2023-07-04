ef92fbe689a6b6fe170a2fc5ca9076d2f4464406
gyasifred <gyasifred@gmail.com>
2023-07-04 10:15:46 +0000

    validate attribute of class

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a Square"""
+
+    def __init__(self, size=0):
+        ''' Initialise a new square class
+        Args:
+            size (int): The size of square
+        '''
+        if not isinstance(size, int):
+            raise TypeError("Size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
