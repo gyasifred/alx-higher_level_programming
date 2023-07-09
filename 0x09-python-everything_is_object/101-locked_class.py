5d54ed40569f1e9f1c8fba53ecc0dc762515d782
gyasifred <gyasifred@gmail.com>
2023-07-09 11:33:02 +0000

    lockedclass

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -9,20 +9,3 @@ class LockedClass:
     """
 
     __slots__ = ['first_name']
-
-    def __setattr__(self, attr, value):
-        """
-        Sets the value of the specified attribute if it is 'first_name', 
-        otherwise raises an AttributeError.
-
-        Parameters:
-            attr (str): The name of the attribute to be set.
-            value: The value to be assigned to the attribute.
-
-        Raises:
-            AttributeError: If the attribute being set is not 'first_name'.
-        """
-        if attr == 'first_name':
-            self.__dict__[attr] = value
-        else:
-            raise AttributeError("Cannot create new instance attributes")
