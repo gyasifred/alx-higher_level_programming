936e465cf22059daa27fce3e7b6543188e602bfe
gyasifred <gyasifred@gmail.com>
2023-07-19 18:00:45 +0000

    publicinstance method print_sorted

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,22 +1,10 @@
 #!/usr/bin/python3
+"""Defines an inherited list class MyList."""
+
 
 class MyList(list):
-    """
-    A custom list class that inherits from the built-in list class.
-
-    Public Method:
-        - print_sorted(): Returns a new list with elements sorted in ascending order (assuming all elements are of type int).
-
-    Note:
-    Since the built-in 'list' class already has a 'sort()' method, which sorts the list in place,
-    we can simply use 'sorted(self)' to return a new sorted list without modifying the original list.
-    """
+    """Implements sorted printing for the built-in list class."""
 
     def print_sorted(self):
-        """
-        Returns a new list with elements sorted in ascending order (assuming all elements are of type int).
-
-        Returns:
-            list: A new list containing the elements of the original list sorted in ascending order.
-        """
-        return sorted(self)
+        """Print a list in sorted ascending order."""
+        print(sorted(self))
