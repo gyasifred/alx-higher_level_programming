ae43bf9527dfc429313417081bb69c76335589ad
gyasifred <gyasifred@gmail.com>
2023-07-19 13:30:57 +0000

    print the attributes and methods of an object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def lookup(obj):
+    """
+    Get a list of attributes and methods of the given object.
+
+    Parameters:
+        obj (object): The object to inspect.
+
+    Returns:
+        list: A list containing the names of attributes and methods of the object.
+    """
+    return list(dir(obj))
