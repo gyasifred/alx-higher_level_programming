8ee5beac1814c7f1559311b663717f1b6ec6c161
gyasifred <gyasifred@gmail.com>
2023-07-19 19:18:46 +0000

    override magi methods using super

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Contains the class MyInt
+"""
+
+
+class MyInt(int):
+    """
+    A custom integer class that inherits from the built-in int class 
+    with inverted == and != operators.
+    """
+
+    def __eq__(self, other):
+        """
+        Override the equality operator (==).
+
+        Parameters:
+            other: The value to compare with.
+
+        Returns:
+            bool: True if the values are not equal; otherwise, False.
+        """
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        """
+        Override the inequality operator (!=).
+
+        Parameters:
+            other: The value to compare with.
+
+        Returns:
+            bool: True if the values are equal; otherwise, False.
+        """
+        return super().__eq__(other)
