a724177b5faa601d2d9e6192d2c0837f67458824
gyasifred <gyasifred@gmail.com>
2023-07-19 18:50:24 +0000

    inherit from base class and use methods

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -8,6 +8,7 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """A representation of a rectangle"""
+
     def __init__(self, width, height):
         """instantiation of the rectangle"""
         self.integer_validator("width", width)
