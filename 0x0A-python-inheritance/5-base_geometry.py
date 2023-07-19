fed19ffe5a25be17da278b2c43d76069ec7bd93f
gyasifred <gyasifred@gmail.com>
2023-07-19 18:32:28 +0000

    empty base class

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""An empty  BaseGeometry"""
+
+
+class BaseGeometry:
+    """
+    An empty class to serve as a base for other geometry-related classes.
+
+    This class does not have any attributes or methods defined, but it can be used as a base class
+    to derive new classes that represent specific geometrical shapes or operations.
+    """
+
+    pass
