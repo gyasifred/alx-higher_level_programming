6974f37bbde84698c0d0a823dcc6a055eb973d6b
gyasifred <gyasifred@gmail.com>
2024-02-22 00:16:50 +0000

    Where and order by clause

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,8 @@
+-- List all records with a score >= 10
+-- Display both the score and the name
+-- Order by score in descending order
+SELECT score, name
+FROM second_table
+WHERE score >= 10
+ORDER BY score DESC;
+
