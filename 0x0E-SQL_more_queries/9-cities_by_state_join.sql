97dac41a27c034a10a782d1f7ee922686ae16e36
gyasifred <gyasifred@gmail.com>
2024-02-28 09:48:34 +0000

    left join

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,6 @@
+-- List all cities along with their corresponding state names
+SELECT cities.id, cities.name, states.name
+FROM cities
+LEFT JOIN states ON states.id = cities.state_id
+ORDER BY cities.id;
+
