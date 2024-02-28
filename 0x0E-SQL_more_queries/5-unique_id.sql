ff56d5551fc9c6e079b10d4b5a51d7ee3800fef0
gyasifred <gyasifred@gmail.com>
2024-02-28 08:41:43 +0000

    create a table

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -3,5 +3,5 @@
 -- id INT with the default value 1 and must be unique
 -- name VARCHAR(256)
 
-CREATE TABLE IF NOT EXISTS unique_id (id INT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
 
